a02fdd39bf6145901638b5ed8f451293b0bd5f0b
sumonpaul55 <sumpaul100@gmail.com>
2024-09-23 00:10:02 +0600

    form wrapper with ts conflict

--- src/components/form/FXForm.tsx
+++ src/components/form/FXForm.tsx
@@ -1,14 +1,28 @@
 import React, { ReactNode } from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
 
 
-type IProps = {
+interface IProps extends TFormConfig {
     children: ReactNode;
+    onSubmit: SubmitHandler<any>
 }
 
+interface TFormConfig {
+    defaultValues?: Record<string, any> | any;
+    resolver?: any
+}
 
-const FXForm = ({ children, onSubmit }: any) => {
-    const method = useForm();
+const FXForm = ({ children, onSubmit, defaultValues, resolver }: IProps) => {
+    let formConfig: TFormConfig = {};
+
+    if (!!defaultValues) { // here !! mean if exist or truthy
+        formConfig = formConfig["defaultValues"]
+    }
+    if (!!resolver) { // here !! mean if exist or truthy
+        formConfig = formConfig["resolver"]
+    }
+
+    const method = useForm(formConfig);
 
     const submitHandler = method.handleSubmit;
     return (
